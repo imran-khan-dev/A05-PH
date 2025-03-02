4711f79a6478507e318b32c03f1c270a2c11bed7
Imran Khan <emrankhanniloy@gmail.com>
2025-03-02 10:35:34 +0600

    discover btn primarily ridirect to fb

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Discover Button
+  document.getElementById("discover-btn").addEventListener("click", () => {
+    window.location.href = "https://www.facebook.com";
+  });
+
+  
+});
