355143eb8c221ec58b0a864bea5e8147941bff84
Imran Khan <emrankhanniloy@gmail.com>
2025-03-02 12:30:26 +0600

    color button current date added

--- script.js
+++ script.js
@@ -3,4 +3,31 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("discover-btn").addEventListener("click", () => {
     window.location.href = "discover.html";
   });
+
+  // Set Current Date
+  const dateElement = document.querySelector(".current-date");
+  const options = {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  };
+  dateElement.textContent = new Date().toLocaleDateString("en-US", options);
+
+  // Function to generate a random color
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  // Event listener for color button
+  document.getElementById("color-btn").addEventListener("click", () => {
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+  });
+  
 });
